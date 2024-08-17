c2f4e79355ba755083899f5f5a87407bcf7504db
Laura Pinto <laurampinto90@gmail.com>
2024-08-17 12:28:39 +0100

    WDPT - FEV2024 Laura

--- src/components/ExponentTwo.jsx
+++ src/components/ExponentTwo.jsx
@@ -1,8 +1,14 @@
-const ExponentTwo = () => (
-   <div className="exponent-counter-container">
-    <p className="exponent-label">n²</p>
-    <p className="exponent-result">2 * 2 = <span className="total">4</span></p>
-  </div>
-);
+const ExponentTwo = ({ count }) => {
+  const result = count * count;
+
+  return (
+    <div className="exponent-counter-container">
+      <p className="exponent-label">{count}^2</p>
+      <p className="exponent-result">
+        {count} * {count} = <span className="total">{result}</span>
+      </p>
+    </div>
+  );
+};
 
 export default ExponentTwo;
